9e724f5ece07ddf2b50f50f8aaa96f9e9fedbb8f
Pavlo-Zubovych <zu10@ukr.net>
2021-12-14 17:55:19 +0200

    Removed node_module folder

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const message = 'Node.js is cool! Paul is driving';
+// index.js
+const validator = require('validator');
 
-console.log(message);
+const validateEmail = (email) => {
+  return validator.isEmail(email);
+};
+
+console.log(
+  'Is mango@mail.com a valid email?: ',
+  validateEmail('mango@mail.com')
+);
+
+console.log(
+  'Is Mangozedog.com a valid email?: ',
+  validateEmail('Mangozedog.com')
+);
